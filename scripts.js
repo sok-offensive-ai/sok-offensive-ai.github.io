bb980efd26e4fa3ceadb315d950758d05cd9e1ce
sok-offensive-ai <saskia.schroeer@uni.li>
2024-05-23 19:58:11 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,33 +16,32 @@ document.addEventListener('DOMContentLoaded', function() {
 
     const createFilter = (tableId, columnIndex) => {
         const table = document.getElementById(tableId);
-        const header = table.getElementsByTagName('thead')[0];
-        const filterRow = document.createElement('tr');
-        const columnCount = table.getElementsByTagName('th').length;
-        for (let i = 0; i < columnCount; i++) {
-            const filterCell = document.createElement('th');
-            if (i === columnIndex) {
-                const select = document.createElement('select');
-                const uniqueValues = new Set();
-                uniqueValues.add('');
-                const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
-                for (const row of rows) {
-                    uniqueValues.add(row.getElementsByTagName('td')[i - 1].textContent);
-                }
-                for (const value of uniqueValues) {
-                    const option = document.createElement('option');
-                    option.value = value;
-                    option.textContent = value;
-                    select.appendChild(option);
-                }
-                select.addEventListener('change', (e) => {
-                    filterTables(columnIndex, e.target.value);
-                });
-                filterCell.appendChild(select);
-            }
-            filterRow.appendChild(filterCell);
+        const filterRow = document.getElementById(`filters${tableId.slice(-1)}`);
+        const filterCell = document.createElement('th');
+        const select = document.createElement('select');
+        const uniqueValues = new Set();
+        uniqueValues.add('');
+        const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+        for (const row of rows) {
+            uniqueValues.add(row.getElementsByTagName('td')[columnIndex - 1].textContent);
         }
-        header.appendChild(filterRow);
+        for (const value of uniqueValues) {
+            const option = document.createElement('option');
+            option.value = value;
+            option.textContent = value;
+            select.appendChild(option);
+        }
+        select.addEventListener('change', (e) => {
+            filterTables(columnIndex, e.target.value);
+        });
+        filterCell.appendChild(select);
+        filterRow.appendChild(filterCell);
     };
 
-    const columnsToFilter = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+    const columnsToFilter = {
+        1: [2], // Year
+        2: [2], // Year
+        3: [2]  // Year
+    };
+
+    const columns = [2, 3, 4, 5, 6, 
